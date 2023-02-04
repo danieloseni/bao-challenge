3438da3dba99ad98f71489d68c755627f28d58df
Daniel Oseni <megoseni0569@gmail.com>
2023-02-04 07:09:27 +0100

    added boilerplate files

---
+++ src/data/newsApi.js
@@ -0,0 +1,9 @@
+export const getNewsItem = async (itemId) => {
+    const item = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
+    return item.data;
+}
+
+export const getNewsList = async () => {
+    const items = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    items.data;    
+}
---
+++ src/hooks/useFetchNews.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const useFetchNews = () => {
+  return (
+    <div>useFetchNews</div>
+  );
+}
+
+export default useFetchNews;
