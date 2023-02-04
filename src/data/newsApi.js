b6ec73dd7ce2800dd123e71c5c6cf4cb287c87ec
Daniel Oseni <megoseni0569@gmail.com>
2023-02-04 15:19:56 +0100

    added: hooks for fetching csv and api news

--- src/data/newsApi.js
+++ src/data/newsApi.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
+
 export const getNewsItem = async (itemId) => {
-    const item = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
-    return item.data;
+  const item = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
+  return item.data;
 }
 
 export const getNewsList = async () => {
-    const items = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-    items.data;    
+  const items = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  return items.data;
 }
--- src/hooks/useFetchNews.js
+++ src/hooks/useFetchNews.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import {useCallback, useState} from 'react';
+import {getNewsItem, getNewsList} from 'data/newsApi';
+import {toast} from 'react-hot-toast';
+import {useDispatch} from 'react-redux';
+import {checkAPINewsFetchStatus, updateNewsIds, updateNewsItems} from 'redux/slices/newsSlice';
+
 
 const useFetchNews = () => {
-  return (
-    <div>useFetchNews</div>
-  );
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const dispatch = useDispatch();
+
+  const batchFetchNewsItems = useCallback(async (ids) => {
+    const totalItems = ids.length;
+    const MAX_CONCURRENT_FETCHES = 10; //We limit it to 10 because that seems to be in the middle ground between responsiveness and speed
+    const newsItems = [];
+    const failedFetches = [];
+    while(ids.length){
+      const currentBatch = ids.splice(0, MAX_CONCURRENT_FETCHES);
+      const results = await Promise.allSettled(currentBatch.map(id => getNewsItem(id)));
+      results.forEach((result, index) => {
+        if(result.status === "fulfilled"){
+          newsItems.push(result.value);
+          return;
+        }
+        failedFetches.push(currentBatch[index]);
+      })
+      const percentLoaded = Math.floor((newsItems.length / totalItems) * 100)
+      setProgress(percentLoaded);      
+    }
+    return newsItems;
+  }, []);
+       
+  const fetchNews = useCallback(async () => {
+    try {
+      setError(false);
+      setLoading(true);      
+      const ids = await getNewsList();
+      //clone the ids array before dispatching because passing the array directly makes it immutable making it impossible to splice it later
+      dispatch(updateNewsIds([...ids]));
+      const remoteNews = await batchFetchNewsItems(ids);
+      const news = [...remoteNews];
+      const sortedNews = news;
+      dispatch(updateNewsItems(sortedNews));
+      dispatch(checkAPINewsFetchStatus())
+      setLoading(false);
+      toast.success('All API news fetched!', {
+        position: 'top-right'
+      })
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  }, [batchFetchNewsItems, dispatch]); 
+
+  return {error, loading, fetchNews, progress}
 }
 
 export default useFetchNews;
