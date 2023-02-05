9341539d434ef7b959166b2087e343fc9b8a67f4
Daniel Oseni <megoseni0569@gmail.com>
2023-02-05 13:03:02 +0100

    added cache policy to speed load time

--- src/data/newsApi.js
+++ src/data/newsApi.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
 
 export const getNewsItem = async (itemId) => {
-  const item = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
-  return item.data;
+  const response = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${itemId}.json`,
+    {cache: "force-cache"}//we use the force-cache policy to remove that overhead, since the resource will most likely not change
+  );
+  const item = await response.json()
+  return item;
 }
 
 export const getNewsList = async () => {
-  const items = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-  return items.data;
+  const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json'); //no cache policy because resource may change
+  const items = await response.json();
+  return items;
 }
