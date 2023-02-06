b12075918c1460a78fb64a13b300ea138926211a
Daniel Oseni <megoseni0569@gmail.com>
2023-02-06 17:09:48 +0100

    removed redundant packages

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,23 @@ import MainLayout from 'layout/Main';
 import Home from 'pages/Main';
 import Admin from 'pages/Admin';
 import {Toaster} from 'react-hot-toast';
-import {QueryClientProvider, QueryClient} from 'react-query';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux'
 
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Toaster />
-          <Header />
-          <MainLayout>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/admin' element={<Admin />} />                    
-            </Routes>
-          </MainLayout>
-        </Router>
-      </QueryClientProvider>
+      <Router>
+        <Toaster />
+        <Header />
+        <MainLayout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/admin' element={<Admin />} />     
+          </Routes>
+        </MainLayout>
+      </Router>
     </Provider>
   );
 }
